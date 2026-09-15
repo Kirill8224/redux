2cc415173ce862b084e1af03497628f702ffd4d4
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-09-15 18:53:08 +0300

    добавил фунгкцию 'skillad' в ui

--- src/SkillsUI/Skills.tsx
+++ src/SkillsUI/Skills.tsx
@@ -1,13 +1,21 @@
-import { Typography, Card } from "@mui/material";
+import { Typography, Card, TextField, Button } from "@mui/material";
 import type {StoreType} from '../skills/store'
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { SkillAdd } from "../skills/skillSlise";
 export function SkillsUI(){
     const Skills= useSelector((state: StoreType)=> state.Skills.items)
+    const dispath= useDispatch()
+    let additem
     return(<>
     <Typography sx={{alignItems: 'center'}} variant="h1">SkillTracker 🚀</Typography>
     <Typography sx={{alignItems: 'center'}} variant="h5">Мои учебные навыки:</Typography>
     {Skills.map((skill, index)=><Card sx={{m: 1}} key={index}>
         <Typography>{skill.title} {skill.isLearned ? ' ✅' : '⏳'}</Typography>
     </Card>)}
+    <TextField onChange={(e)=>{additem= e.target.value}} label='новый навык' placeholder="печатайте"></TextField>
+    <Button onClick={()=>{dispath(SkillAdd(additem))}}>добавить</Button>
     </>)
 }
+
+
